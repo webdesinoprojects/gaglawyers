afdd07fc69f2b097fe5064fe07179de9e871833b
krishna-dtu <kj20130284870@gmail.com>
2026-05-25 21:19:53 +0530

    5000 port listening

--- backend/server.js
+++ backend/server.js
@@ -113,10 +113,8 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
 
 module.exports = app;
